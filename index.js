bf70d389c3a4a5426b3367f8ff673fca8522589a
Francisco López <donflopez@gmail.com>
2017-01-04 17:11:26 +0100

    Add cap and joint references to line

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ module.exports.load = function(version) {
     if (versions.indexOf(version) <= -1) {
 	throw new Error("Unknown mapnik-reference version: '" + version + "'");
     }
-    var ref = require(path.join(__dirname, version, 'reference.json'));
-    var ds_path = path.join(__dirname, version, 'datasources.json');
-    if (existsSync(ds_path)) {
-	ref.datasources = require(ds_path).datasources;
-    }
+    var ref = require('./1.0.0/reference.json');
+ //    var ds_path = path.join(__dirname, version, 'datasources.json');
+ //    if (existsSync(ds_path)) {
+	// ref.datasources = require(ds_path).datasources;
+ //    }
     return ref;
 }
