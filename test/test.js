43e4600c5f9bc420b2b188462e313a71189c76e1
IagoLast <iago.lastra@gmail.com>
2017-07-24 12:26:26 +0200

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,45 +1,10 @@
-var assert = require('assert');
-var mapnikref = require('../');
-var glob = require('glob');
-var path = require('path');
+var expect = require('chai').expect
 
-describe('datasources', function() {
-    mapnikref.versions.forEach(function(key) {
-        var spec = mapnikref.load(key);
-        if (spec.datasources) {
-            it('show provide metadata for '+key, function() {
-                var ds_spec = spec.datasources['postgis'];
-                assert.ok(ds_spec['table'].type,'string');
-                assert.equal(ds_spec['table'].required,true);
-            });
-        } else {
-            it.skip('show provide metadata for '+key, function() {});
-        }
+describe('1.0.0', function () {
+    var reference = require('../1.0.0/reference.json');
+    describe('Should not have unsupported properties', function () {
+        it('markers.file', function () {
+            expect(reference.symbolizers.markers.file).to.be.undefined;
+        });
     });
 });
-
-describe('styles', function() {
-    mapnikref.versions.forEach(function(key) {
-        (function(key) {
-            it('report correct version for '+key, function() {
-                assert.equal(mapnikref.load(key).version,key);
-            });
-        })(key)
-    })
-
-    it('reveal new property in Mapnik 2.3.0', function() {
-        assert.ok(mapnikref.load('2.3.0').symbolizers.markers['geometry-transform']);
-        assert.ok(!mapnikref.load('2.0.0').symbolizers.markers['geometry-transform']);
-    });
-});
-
-
-describe('versions', function() {
-    it('show report all versions', function() {
-        var versions = glob.sync("./*.*.*");
-        versions = versions.map(function(e) { return path.basename(e) })
-        assert.deepEqual(versions,mapnikref.versions);
-    });
-});
-
-
